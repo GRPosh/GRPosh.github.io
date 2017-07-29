da1ce751e167ab77c77501c1cada6ca9c926d970
Thomas Malkewitz <dotps1@outlook.com>
2017-07-28 20:37:19 -0400

    BAH!

--- js/main.js
+++ js/main.js
@@ -21,6 +21,10 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
