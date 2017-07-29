6afe0d39d075e321db618238e5d04581b2f42a86
Thomas Malkewitz <dotps1@outlook.com>
2017-07-28 20:22:16 -0400

    Dont even know any more!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+---
+layout: null
+sitemap:
+  exclude: 'yes'
+---
+
 $(document).ready(function () {
   $('a.events-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
@@ -12,6 +18,10 @@ $(document).ready(function () {
   })
 
   if (window.location.hash && window.location.hash == '#events') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
